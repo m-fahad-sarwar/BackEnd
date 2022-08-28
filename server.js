120180ef31806366cd0cf6bb8560a379a74dab1b
M Fahad Sarwar <sarwarf271@gmail.com>
2022-08-29 00:02:08 +0500

    Updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 var bodyParser = require("body-parser");
 require('dotenv').config();
 
-const port =  process.env.PORT || 5000
+const port =  process.env.PORT || 3000
 const app = express();
 var cors = require('cors')
 
@@ -16,6 +16,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 setupDB()
+
+
+
 
 app.use(cors())
 app.use('/auth', authRoutes)
